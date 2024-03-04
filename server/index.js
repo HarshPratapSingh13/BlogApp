fc221feb1ed5299cfdaccf239a22f09d67eaa61e
HARSH PRATAP SINGH <hpsingh9536@gmail.com>
2024-03-04 16:40:28 +0530

    Added both scehmas and connected mongodb

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
 dotenv.config();
 
 import userRoute from "./routes/user.js";
 import authRoute from "./routes/auth.js";
-import postRoute from "./route/post.js";
+import postRoute from "./routes/post.js";
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(console.log("✅ Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 app.get("/" , (req, res) => {
     res.send(`hello world`);
--- server/models/post.js
+++ server/models/post.js
@@ -1,5 +1,36 @@
 import mongoose from "mongoose";
 
-const PostSchema = new mongoose.Schema({
+const postSchema = new mongoose.Schema({
+
+
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+    photo: {
+        type: String,
+        required: false,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    categories: {
+        type: Array,
+        required: false,
+    },
+    },
+    {
+         timestamps: true
+    }   
     
-})
+)
+
+
+const PostSchema = mongoose.model('Post' , postSchema);
+export default PostSchema ; 
